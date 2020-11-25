e4171417516300b711a434eb8ad7b23e44cce677
Andrey ``Bass'' Shcheglov <andrewbass@gmail.com>
2020-11-25 12:55:35 +0300

    Enable upcase-region and xterm-mouse-mode

--- .emacs.el
+++ .emacs.el
@@ -15,10 +15,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(xterm-mouse-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:background "#fff7e9" :foreground "black")))))
+(put 'upcase-region 'disabled nil)
